6a082a714c8e01ddd495b03955a86047bdf3bb99
Jan Boon <jan.boon@kaetemi.be>
2026-04-08 08:08:15 +0800

    feat(eve5): use lvgl vram support in eve5 draw unit

--- src/lv_conf_internal.h
+++ src/lv_conf_internal.h
@@ -1134,6 +1134,15 @@
     #endif
 #endif
 
+/* Use EVE BT820 GPU through EVE HAL. */
+#ifndef LV_USE_DRAW_EVE5
+    #ifdef CONFIG_LV_USE_DRAW_EVE5
+        #define LV_USE_DRAW_EVE5 CONFIG_LV_USE_DRAW_EVE5
+    #else
+        #define LV_USE_DRAW_EVE5 0
+    #endif
+#endif
+
 /* VRAM buffer residency support for hardware draw units. */
 #ifndef LV_USE_DRAW_VRAM
     #ifdef CONFIG_LV_USE_DRAW_VRAM
@@ -1141,6 +1150,12 @@
     #else
         #define LV_USE_DRAW_VRAM 0
     #endif
+#endif
+
+/* LV_USE_DRAW_EVE5 requires LV_USE_DRAW_VRAM */
+#if LV_USE_DRAW_EVE5 && !LV_USE_DRAW_VRAM
+    #undef LV_USE_DRAW_VRAM
+    #define LV_USE_DRAW_VRAM 1
 #endif
 
 /*=======================
